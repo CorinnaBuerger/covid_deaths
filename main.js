e3c3815e876c5c780d7eb3bbaa49eec70d95a084
Corinna Bürger <corinna_buerger@yahoo.de>
2020-09-26 20:44:14 +0200

    new approaches in interactive plot changes

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log("select: value=" + this.value)
+console.log(cb_obj.value, "got selected");
+console.log(df_dict[cb_obj.value]);
+graph.data.x = df_dict["dates"];
+graph.data.y = df_dict[cb_obj.value];
+graph.change.emit();
+
