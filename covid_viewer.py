95fe9b521f3b05f09f7c8968c0ddc00afc6d4d19
Corinna Bürger <corinna_buerger@yahoo.de>
2020-11-03 22:26:21 +0100

    splitted plot creation in multiple functions

--- covid_viewer.py
+++ covid_viewer.py
@@ -1,6 +1,6 @@
 from bokeh.layouts import row, column                        # type: ignore
 from bokeh.models import ColumnDataSource, CustomJS, Select  # type: ignore
-from bokeh.models import HoverTool                           # type: ignore
+from bokeh.models import HoverTool, NumeralTickFormatter     # type: ignore
 from bokeh.plotting import figure                            # type: ignore
 from bokeh.io import output_file, show, save                 # type: ignore
 from bokeh.util.browser import view
@@ -29,14 +29,17 @@ class CovidData():
 
         self.start = self.df_total.columns[4]
         self.today = self.df_total.columns[-1]
-        self.dates = pd.date_range(self.start, self.today).date
+        self.dates = []
+        self.dates_str = []
 
         # no country selected yet
         self.selected = None
         self.world_data = None
 
-        # will be filled and transformed into self.df_daily
+        # will be filled 
         self.daily_deaths = {}
+        self.df_dict_daily = {}
+        self.df_dict_total = {}
 
         # DataFrame for daily death cases
         self.df_daily = self.get_daily_deaths()
@@ -135,21 +138,130 @@ class CovidData():
         s_daily = s_daily.rename(columns={col_names[0]: name})
         self.selected = s_daily
 
-    def plot_selected_country(self, name, output, module="bokeh"):
+    def create_plot(self, name, dic, title, css_class):
+        dic["selected"] = dic[name]
+
+        SOURCE = ColumnDataSource(data=dic)
+        TITLE = title
+        CSS_CLASS = [css_class]
+        XAXIS_LABEL = "Date"
+        YAXIS_LABEL = "Death Cases"
+        HEIGHT = 600
+        WIDTH= 760
+        SIZE = 1
+        CIRCLE_SIZE = 12
+        COLOR = "blue"
+        TOOLTIPS = [("Date", "@dates_str"), 
+                    ("Deaths of selected country", "@selected"), 
+                    ("Deaths worldwide", "@World")]
+        TOOLS = [HoverTool(tooltips=TOOLTIPS), "pan", 
+                 "wheel_zoom", "box_zoom", "reset"]
+
+        # create plot
+        p = figure(x_axis_type="datetime", title=TITLE, 
+                   plot_height=HEIGHT, tools=TOOLS, width=WIDTH,
+                   css_classes=CSS_CLASS)
+
+        p.vbar(x='dates', top="selected", color=COLOR, line_width=SIZE, source=SOURCE)
+        p.circle(x='dates', y="selected", color=COLOR, size=CIRCLE_SIZE, source=SOURCE, 
+                 fill_alpha=0, line_alpha=0)
+        p.yaxis.axis_label = YAXIS_LABEL
+        p.xaxis.axis_label = XAXIS_LABEL
+        p.yaxis.formatter=NumeralTickFormatter(format="0a")
+
+        return p, SOURCE
+
+    def create_dropdown(self, name, source_daily, source_total):
+
+        # dates can't be sorted like this, so it has to be removed for this step
+        self.df_dict_total.pop("dates")
+        self.df_dict_total.pop("dates_str")
+        sort_options = sorted(self.df_dict_total.items(), key=lambda x: x[1][-1],
+                              reverse=True)
+        options = []
+        for tpl in sort_options:
+            total_deaths_list = list(str(tpl[1][-1]))
+            total_deaths_str_sep = ""
+            for i, num in enumerate(total_deaths_list):
+                total_deaths_str_sep += num
+                if i == len(total_deaths_list)-1:
+                    continue
+                elif len(total_deaths_list) % 3 == 0:
+                    if i % 3 == 2:
+                        total_deaths_str_sep += ","
+                elif len(total_deaths_list) % 3 == 1:
+                    if i % 3 == 0:
+                        total_deaths_str_sep += ","
+                elif len(total_deaths_list) % 3 == 2:
+                    if i % 3 == 1:
+                        total_deaths_str_sep += ","
+            if tpl[0] == name:
+                selected_total_deaths_sep = total_deaths_str_sep
+            options.append(f"{tpl[0]}: {total_deaths_str_sep} total deaths")
+
+        options.remove(f"selected: {selected_total_deaths_sep} total deaths")
+
+        # dates need to be added again since they were removed for sorting the options
+        self.df_dict_total["dates"] = self.dates
+        self.df_dict_total["dates_str"] = self.dates_str
+
+        select = Select(title="Select a country", 
+                        value=f"{name}: {selected_total_deaths_sep} total deaths",
+                        options=options, sizing_mode="scale_width")
+        with open("main.js", "r") as f:
+            select.js_on_change("value", CustomJS(
+                args=dict(source_d=source_daily, source_t=source_total,
+                          df_dict_t=self.df_dict_total, df_dict_d=self.df_dict_daily,
+                          ), code=f.read()))
+
+        return select
+
+
+    def plot_with_bokeh(self, name, output):
+        pd, source_d = self.create_plot(name, self.df_dict_daily, "Daily Deaths", "we-need-this-for-manip")
+        pt, source_t = self.create_plot(name, self.df_dict_total, "Total Deaths", "we-need-this-for-manip-total")
+
+        select = self.create_dropdown(name, source_d, source_t)
+
+        with open("template.html", "r") as f:
+            template = f.read()
+
+        plots = column(pd, pt)
+        save(column(select, plots), template=template)
+        view(output)
+
+    def plot_with_mpl(self, name):
+        death_cases = []
+        death_cases_world = []
+        # cave: only for daily
+        for sub_arr in self.selected.values:
+            death_cases.append(sub_arr[0])
+        for sub_arr in self.world_data_daily.values:
+            death_cases_world.append(sub_arr[0])
+
+        fig, ax = plt.subplots()
+        date_format = DateFormatter("%d %b %Y")
+        world_plot = ax.bar(self.dates, death_cases_world,
+                            bottom=0, color="lightgray")
+        country_plot = ax.bar(self.dates, death_cases, bottom=0)
+        ax.set(xlabel="Date", ylabel="Death Cases")
+        ax.xaxis.set_major_formatter(date_format)
+        fig.subplots_adjust(bottom=0.175)
+        plt.xticks(rotation=35, fontsize=7)
+        plt.legend((world_plot[0], country_plot[0]),
+                   ("Worldwide", "{}".format(name)))
+        plt.show()
+
+    def fill_dict_for_source(self, df, dic, name):
         if self.selected is None:
             raise ValueError("no country selected")
 
         # create dictionary out of df that can be put into JS function
-        grouped_df_d = self.df_daily.groupby("Country/Region", sort=False)
-        grouped_df_t = self.df_total.groupby("Country/Region", sort=False)
-        grouped_list_d = grouped_df_d.apply(lambda x: x.to_dict(orient="list"))
-        grouped_list_t = grouped_df_t.apply(lambda x: x.to_dict(orient="list"))
-        df_dict_nested_d = grouped_list_d.to_dict()
-        df_dict_nested_t = grouped_list_t.to_dict()
-        df_dict_daily = {}
-        df_dict_total = {}
+        grouped_df = df.groupby("Country/Region", sort=False)
+        grouped_list = grouped_df.apply(lambda x: x.to_dict(orient="list"))
+        df_dict_nested = grouped_list.to_dict()
         keys_to_ignore = ["Province/State", "Country/Region", "Lat", "Long"]
-        for key, value in df_dict_nested_d.items():
+        for key, value in df_dict_nested.items():
             helper_list = []
             for key_two, value_two in value.items():
                 if key_two in keys_to_ignore:
@@ -157,162 +269,28 @@ class CovidData():
                 else:
                     # sums up countries that occur multiple times
                     helper_list.append(sum(value_two))
-            df_dict_daily[key] = helper_list
-        for key, value in df_dict_nested_t.items():
-            helper_list = []
-            for key_two, value_two in value.items():
-                if key_two in keys_to_ignore:
-                    continue
-                else:
-                    # sums up countries that occur multiple times
-                    helper_list.append(sum(value_two))
-            df_dict_total[key] = helper_list
+            dic[key] = helper_list
 
-        dates = []
-        dates_str = []
+        # needs to be cleared since it would be doubled otherwise 
+        self.dates = []
+        self.dates_str = []
         for date_str in self.selected.index:
             date_obj = datetime.strptime(date_str, '%m/%d/%y')
-            dates.append(date_obj)
+            self.dates.append(date_obj)
             date_str_new = datetime.strptime(date_str, '%m/%d/%y').strftime('%d %b %Y')
-            dates_str.append(date_str_new)
-        df_dict_daily["dates"] = dates
-        df_dict_total["dates"] = dates
-        df_dict_daily["dates_str"] = dates_str
-        df_dict_total["dates_str"] = dates_str
+            self.dates_str.append(date_str_new)
+        dic["dates"] = self.dates
+        dic["dates_str"] = self.dates_str
+
+    def plot_selected_country(self, name, output, module):
+        self.fill_dict_for_source(self.df_daily, self.df_dict_daily, name) 
+        self.fill_dict_for_source(self.df_total, self.df_dict_total, name) 
 
         if module == "bokeh":
-
-            # also necessary to make it compatible with JS function
-            df_dict_daily["selected"] = df_dict_daily[name]
-            df_dict_total["selected"] = df_dict_total[name]
-            source_daily = ColumnDataSource(data=df_dict_daily)
-            source_total = ColumnDataSource(data=df_dict_total)
-
-            # create two plots
-            XAXIS_LABEL = "Date"
-            YAXIS_LABEL = "Death Cases"
-            LEGEND_LOC = "top_left"
-            TOOLTIPS = [("Date", "@dates_str"), 
-                        ("Deaths of selected country", "@selected"), 
-                        ("Deaths worldwide", "@World")]
-            TOOLS = [HoverTool(tooltips=TOOLTIPS), "pan", 
-                     "wheel_zoom", "box_zoom", "reset"]
-            HEIGHT = 600
-            WIDTH= 760
-            SIZE = 1
-            CIRCLE_SIZE = 12
-
-            colors = ["lightgray", "blue"]
-            pd = figure(x_axis_type="datetime", title="Daily Deaths", 
-                        plot_height=HEIGHT, tools=TOOLS, width=WIDTH,
-                        css_classes=["we-need-this-for-manip"], name="d_deaths")
-            pt = figure(x_axis_type="datetime", title="Total Deaths", 
-                        plot_height=HEIGHT, tools=TOOLS, width=WIDTH,
-                        css_classes=["we-need-this-for-manip-total"], name="t_deaths")
-
-            # pd.vbar(x='dates', top="World", color=colors[0], line_width=SIZE,
-            #         source=source_daily, legend_label="Worldwide")
-            pd.vbar(x='dates', top="selected", color=colors[1], line_width=SIZE,
-                    source=source_daily, legend_label="Selected Country")
-            # pd.circle(x='dates', y="World", color=colors[0], size=CIRCLE_SIZE,
-            #           source=source_daily, legend_label="Worldwide", fill_alpha=0,
-            #           line_alpha=0)
-            pd.circle(x='dates', y="selected", color=colors[1], size=CIRCLE_SIZE,
-                      source=source_daily, legend_label="Selected Country", fill_alpha=0,
-                      line_alpha=0)
-            pd.legend.location = LEGEND_LOC
-            pd.yaxis.axis_label = YAXIS_LABEL
-            pd.xaxis.axis_label = XAXIS_LABEL
-
-            # pt.vbar(x='dates', top="World", color=colors[0], line_width=SIZE,
-            #         source=source_total, legend_label="Worldwide")
-            pt.vbar(x='dates', top="selected", color=colors[1], line_width=SIZE,
-                    source=source_total, legend_label="Selected Country")
-            # pt.circle(x='dates', y="World", color=colors[0], size=CIRCLE_SIZE,
-            #           source=source_total, legend_label="Worldwide", fill_alpha=0, 
-            #           line_alpha=0)
-            pt.circle(x='dates', y="selected", color=colors[1], size=CIRCLE_SIZE,
-                      source=source_total, legend_label="Selected Country", fill_alpha=0,
-                      line_alpha=0)
-            pt.legend.location = LEGEND_LOC
-            pt.yaxis.axis_label = YAXIS_LABEL
-            pt.xaxis.axis_label = XAXIS_LABEL
-
-            output_file(output)
-
-            # dropdown menu
-
-            # dates can't be sorted like this, so it has to be removed for this step
-            df_dict_total.pop("dates")
-            df_dict_total.pop("dates_str")
-            sort_options = sorted(df_dict_total.items(), key=lambda x: x[1][-1],
-                                  reverse=True)
-            options = []
-            for tpl in sort_options:
-                total_deaths_list = list(str(tpl[1][-1]))
-                total_deaths_str_sep = ""
-                for i, num in enumerate(total_deaths_list):
-                    total_deaths_str_sep += num
-                    if i == len(total_deaths_list)-1:
-                        continue
-                    elif len(total_deaths_list) % 3 == 0:
-                        if i % 3 == 2:
-                            total_deaths_str_sep += ","
-                    elif len(total_deaths_list) % 3 == 1:
-                        if i % 3 == 0:
-                            total_deaths_str_sep += ","
-                    elif len(total_deaths_list) % 3 == 2:
-                        if i % 3 == 1:
-                            total_deaths_str_sep += ","
-                if tpl[0] == name:
-                    selected_total_deaths_sep = total_deaths_str_sep
-                options.append(f"{tpl[0]}: {total_deaths_str_sep} total deaths")
-
-            options.remove(f"selected: {selected_total_deaths_sep} total deaths")
-
-            df_dict_total["dates"] = dates
-            df_dict_total["dates_str"] = dates_str
-
-            select = Select(title="Select a country", 
-                            value=f"{name}: {selected_total_deaths_sep} total deaths",
-                            options=options, sizing_mode="scale_width")
-            with open("main.js", "r") as f:
-                select.js_on_change("value", CustomJS(
-                    args=dict(source_d=source_daily, source_t=source_total,
-                              df_dict_t=df_dict_total, df_dict_d=df_dict_daily,
-                              ), code=f.read()))
-
-            plots = column(pd, pt)
-            # show(column(select, plots)) 
-
-            with open("template.html", "r") as f:
-                template = f.read()
-
-            save(column(select, plots), template=template)
-            view(output)
-
+            self.plot_with_bokeh(name, output)
 
         if module == "mpl":
-            death_cases = []
-            death_cases_world = []
-            # cave: only for daily
-            for sub_arr in self.selected.values:
-                death_cases.append(sub_arr[0])
-            for sub_arr in self.world_data_daily.values:
-                death_cases_world.append(sub_arr[0])
-
-            fig, ax = plt.subplots()
-            date_format = DateFormatter("%d %b %Y")
-            world_plot = ax.bar(dates, death_cases_world,
-                                bottom=0, color="lightgray")
-            country_plot = ax.bar(dates, death_cases, bottom=0)
-            ax.set(xlabel="Date", ylabel="Death Cases")
-            ax.xaxis.set_major_formatter(date_format)
-            fig.subplots_adjust(bottom=0.175)
-            plt.xticks(rotation=35, fontsize=7)
-            plt.legend((world_plot[0], country_plot[0]),
-                       ("Worldwide", "{}".format(name)))
-            plt.show()
+            self.plot_with_mpl(name)
 
     @staticmethod
     def update_local_data():
