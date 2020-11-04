3738c53e66537baf76d253e50d8249a25b15bcc6
Corinna Bürger <corinna_buerger@yahoo.de>
2020-11-04 18:27:53 +0100

    join together infections and deaths in one class

--- covid_viewer.py
+++ covid_viewer.py
@@ -1,10 +1,10 @@
+from bokeh.io import output_file, show, save                 # type: ignore
 from bokeh.layouts import row, column                        # type: ignore
 from bokeh.models import ColumnDataSource, CustomJS, Select  # type: ignore
 from bokeh.models import HoverTool, NumeralTickFormatter     # type: ignore
 from bokeh.plotting import figure                            # type: ignore
-from bokeh.io import output_file, show, save                 # type: ignore
-from bokeh.util.browser import view
-from datetime import datetime
+from bokeh.util.browser import view                          # type: ignore
+from datetime import datetime                                # type: ignore
 from matplotlib.dates import DateFormatter                   # type: ignore
 from sys import argv, exit
 import os
@@ -12,43 +12,68 @@ import matplotlib.pyplot as plt                              # type: ignore
 import pandas as pd                                          # type: ignore
 import requests
 
-usage_msg = ("""Usage: covid_viewer <country> <bokeh/mpl> [<output_file>] [--update] [--help]
+usage_msg = ("""Usage: covid_viewer <country> <bokeh/mpl> <input_csv> <output_file> [--update] [--help]
     --update\t\tupdate the local COVID data copy from JHU
     --help\t\tdisplay this help message
 
 Copyright (c) 2020 by Corinna Buerger""")
 
 JHU_RESPONSE_MIN_LENGTH = 100000
-JHU_UPDATED_DATA_FILENAME = "covid_deaths.csv"  # NOTE: potentially overrides
 
 
 class CovidData():
-    def __init__(self, infile="covid_deaths.csv"):
-        # pd.DataFrame for total death cases
-        self.df_total = pd.read_csv(infile)
-
-        self.start = self.df_total.columns[4]
-        self.today = self.df_total.columns[-1]
-        self.dates = []
-        self.dates_str = []
-
-        # no country selected yet
-        self.selected = None
-        self.world_data = None
+    def __init__(self, infile):
+        self.INFECTIONS = False
+        self.DEATHS = False
 
         # will be filled 
-        self.daily_deaths = {}
+        self.daily_cases = {}
         self.df_dict_daily = {}
         self.df_dict_total = {}
 
-        # DataFrame for daily death cases
-        self.df_daily = self.get_daily_deaths()
+        # pd.DataFrame for total cases
+        self.df_total = pd.read_csv(infile)
+        self.df_daily = self.get_daily_cases()
 
-        # adds worldwide death cases to both DataFrames
-        self.get_world_deaths()
+        # adds worldwide cases to both DataFrames
+        self.get_world_cases()
 
-    def get_world_deaths(self):
-        # append to DataFrame for total deaths
+        # not created yet
+        self.dates = None
+        self.dates_str = None
+
+        # no country selected yet
+        self.selected = None
+
+        if "deaths" in infile:
+            self.DEATHS = True
+        else:
+            self.INFECTIONS = True
+
+
+    def get_daily_cases(self):
+        for column_in_df in self.df_total.columns:
+            self.daily_cases[column_in_df] = []
+
+        for row_idx in range(0, len(self.df_total)):
+            for col_idx in range(0, len(self.df_total.columns)):
+                column = self.df_total.columns[col_idx]
+                if col_idx <= 4:
+                    # concerns all columns that do not contain data of 
+                    # cases as well as for the first day of documentation
+                    self.daily_cases[column].append(self.df_total.
+                                                     iloc[row_idx, col_idx])
+                else:
+                    # calculates the difference between today and yesterday
+                    self.daily_cases[column].append(
+                            self.df_total.iloc[row_idx, col_idx] -
+                            self.df_total.iloc[row_idx, col_idx-1])
+
+        # created dict can now be transformed into a DataFrame
+        return pd.DataFrame(self.daily_cases)
+
+    def get_world_cases(self):
+        # append to DataFrame for total cases
         world_total = {}
         for column_in_df in self.df_total.columns:
             world_total[column_in_df] = None
@@ -56,26 +81,26 @@ class CovidData():
         for col_idx in range(0, len(self.df_total.columns)):
             column = self.df_total.columns[col_idx]
             if col_idx < 4:
-                # here is no data for death cases
+                # here is no data for cases
                 world_total[column] = "World"
             else:
                 for row_idx in range(0, len(self.df_total)):
                     if row_idx == 0:
                         # for the first row (country) in each column (day)
-                        # the death cases will be assigned
+                        # the cases will be assigned
                         world_total[column] = self.df_total.iloc[row_idx,
                                                                  col_idx]
                     else:
                         # for all the other rows (countries) in each column
-                        # (day) the death cases will be added to the previous
+                        # (day) the cases will be added to the previous
                         # one
                         world_total[column] += self.df_total.iloc[row_idx,
                                                                   col_idx]
 
         self.df_total = self.df_total.append(world_total, ignore_index=True)
 
-        # append to DataFrame for daily deaths (works just like for total
-        # deaths but uses self.df_daily)
+        # append to DataFrame for daily cases (works just like for total
+        # cases but uses self.df_daily)
         world_daily = {}
         for column in self.df_daily.columns:
             world_daily[column] = None
@@ -94,27 +119,6 @@ class CovidData():
                                                                   col_idx]
 
         self.df_daily = self.df_daily.append(world_daily, ignore_index=True)
-
-    def get_daily_deaths(self):
-        for column_in_df in self.df_total.columns:
-            self.daily_deaths[column_in_df] = []
-
-        for row_idx in range(0, len(self.df_total)):
-            for col_idx in range(0, len(self.df_total.columns)):
-                column = self.df_total.columns[col_idx]
-                if col_idx <= 4:
-                    # concerns all columns that do not contain data of death
-                    # cases as well as for the first day of documentation
-                    self.daily_deaths[column].append(self.df_total.
-                                                     iloc[row_idx, col_idx])
-                else:
-                    # calculates the difference between today and yesterday
-                    self.daily_deaths[column].append(
-                            self.df_total.iloc[row_idx, col_idx] -
-                            self.df_total.iloc[row_idx, col_idx-1])
-
-        # created dict can now be transformed into a DataFrame
-        return pd.DataFrame(self.daily_deaths)
 
     def select_country(self, name="US"):
         s_daily = self.df_daily[self.df_daily["Country/Region"]
@@ -141,21 +145,27 @@ class CovidData():
     def create_plot(self, name, dic, title, css_class):
         dic["selected"] = dic[name]
 
+        if self.DEATHS:
+            YAXIS = "Deaths"
+            COLOR = "blue"
+        else:
+            YAXIS = "Infections"
+            COLOR = "red"
         SOURCE = ColumnDataSource(data=dic)
-        TITLE = title
+        TITLE = title + " " + YAXIS
         CSS_CLASS = [css_class]
         XAXIS_LABEL = "Date"
-        YAXIS_LABEL = "Death Cases"
+        YAXIS_LABEL = YAXIS
         HEIGHT = 600
         WIDTH= 760
         SIZE = 1
         CIRCLE_SIZE = 12
-        COLOR = "blue"
         TOOLTIPS = [("Date", "@dates_str"), 
-                    ("Deaths of selected country", "@selected"), 
-                    ("Deaths worldwide", "@World")]
+                    (f"{YAXIS} of selected country", "@selected"), 
+                    (f"{YAXIS} worldwide", "@World")]
         TOOLS = [HoverTool(tooltips=TOOLTIPS), "pan", 
                  "wheel_zoom", "box_zoom", "reset"]
+
 
         # create plot
         p = figure(x_axis_type="datetime", title=TITLE, 
@@ -173,6 +183,8 @@ class CovidData():
 
     def create_dropdown(self, name, source_daily, source_total):
 
+        # get options for dropdown
+
         # dates can't be sorted like this, so it has to be removed for this step
         self.df_dict_total.pop("dates")
         self.df_dict_total.pop("dates_str")
@@ -180,71 +192,81 @@ class CovidData():
                               reverse=True)
         options = []
         for tpl in sort_options:
-            total_deaths_list = list(str(tpl[1][-1]))
-            total_deaths_str_sep = ""
-            for i, num in enumerate(total_deaths_list):
-                total_deaths_str_sep += num
-                if i == len(total_deaths_list)-1:
+            total_cases_list = list(str(tpl[1][-1]))
+            total_cases_str_sep = ""
+            for i, num in enumerate(total_cases_list):
+                total_cases_str_sep += num
+                if i == len(total_cases_list)-1:
                     continue
-                elif len(total_deaths_list) % 3 == 0:
+                elif len(total_cases_list) % 3 == 0:
                     if i % 3 == 2:
-                        total_deaths_str_sep += ","
-                elif len(total_deaths_list) % 3 == 1:
+                        total_cases_str_sep += ","
+                elif len(total_cases_list) % 3 == 1:
                     if i % 3 == 0:
-                        total_deaths_str_sep += ","
-                elif len(total_deaths_list) % 3 == 2:
+                        total_cases_str_sep += ","
+                elif len(total_cases_list) % 3 == 2:
                     if i % 3 == 1:
-                        total_deaths_str_sep += ","
+                        total_cases_str_sep += ","
             if tpl[0] == name:
-                selected_total_deaths_sep = total_deaths_str_sep
-            options.append(f"{tpl[0]}: {total_deaths_str_sep} total deaths")
+                selected_total_cases_sep = total_cases_str_sep
+            options.append(f"{tpl[0]}: {total_cases_str_sep} total cases")
 
-        options.remove(f"selected: {selected_total_deaths_sep} total deaths")
+        options.remove(f"selected: {selected_total_cases_sep} total cases")
 
         # dates need to be added again since they were removed for sorting the options
         self.df_dict_total["dates"] = self.dates
         self.df_dict_total["dates_str"] = self.dates_str
 
+        # create dropdown
+
         select = Select(title="Select a country", 
-                        value=f"{name}: {selected_total_deaths_sep} total deaths",
+                        value=f"{name}: {selected_total_cases_sep} total cases",
                         options=options, sizing_mode="scale_width")
+
         with open("main.js", "r") as f:
-            select.js_on_change("value", CustomJS(
-                args=dict(source_d=source_daily, source_t=source_total,
-                          df_dict_t=self.df_dict_total, df_dict_d=self.df_dict_daily,
-                          ), code=f.read()))
+            JS_function = CustomJS(args=dict(source_d=source_daily, source_t=source_total, 
+                                             df_dict_t=self.df_dict_total, 
+                                             df_dict_d=self.df_dict_daily), code=f.read())
+            select.js_on_change("value", JS_function)
 
         return select
 
 
     def plot_with_bokeh(self, name, output):
-        pd, source_d = self.create_plot(name, self.df_dict_daily, "Daily Deaths", "we-need-this-for-manip")
-        pt, source_t = self.create_plot(name, self.df_dict_total, "Total Deaths", "we-need-this-for-manip-total")
+        pd, source_d = self.create_plot(name, self.df_dict_daily, 
+                                        "Daily", "we-need-this-for-manip")
+        pt, source_t = self.create_plot(name, self.df_dict_total, 
+                                        "Total", "we-need-this-for-manip-total")
 
         select = self.create_dropdown(name, source_d, source_t)
 
         with open("template.html", "r") as f:
             template = f.read()
 
+        output_file(output)
         plots = column(pd, pt)
         save(column(select, plots), template=template)
         view(output)
 
     def plot_with_mpl(self, name):
-        death_cases = []
-        death_cases_world = []
+        if self.DEATHS:
+            ylabel = "Death Cases"
+        else:
+            ylabel = "Infections"
+        cases = []
+        cases_world = []
         # cave: only for daily
         for sub_arr in self.selected.values:
-            death_cases.append(sub_arr[0])
+            cases.append(sub_arr[0])
         for sub_arr in self.world_data_daily.values:
-            death_cases_world.append(sub_arr[0])
+            cases_world.append(sub_arr[0])
 
         fig, ax = plt.subplots()
         date_format = DateFormatter("%d %b %Y")
-        world_plot = ax.bar(self.dates, death_cases_world,
+        world_plot = ax.bar(self.dates, cases_world,
                             bottom=0, color="lightgray")
-        country_plot = ax.bar(self.dates, death_cases, bottom=0)
-        ax.set(xlabel="Date", ylabel="Death Cases")
+        country_plot = ax.bar(self.dates, cases, bottom=0)
+        ax.set(xlabel="Date", ylabel=ylabel)
         ax.xaxis.set_major_formatter(date_format)
         fig.subplots_adjust(bottom=0.175)
         plt.xticks(rotation=35, fontsize=7)
@@ -293,13 +315,13 @@ class CovidData():
             self.plot_with_mpl(name)
 
     @staticmethod
-    def update_local_data():
+    def update_local_data(source, input_csv):
         base_url = "https://raw.githubusercontent.com/"
         url = (base_url +
                "CSSEGISandData/COVID-19/" +
                "master/csse_covid_19_data/" +
                "csse_covid_19_time_series/" +
-               "time_series_covid19_deaths_global.csv")
+               f"time_series_covid19_{source}_global.csv")
 
         response = requests.get(url)
 
@@ -308,11 +330,10 @@ class CovidData():
             if len(content) < JHU_RESPONSE_MIN_LENGTH:
                 print("got a very short response, aborting")
                 exit(1)
-            csv_file = open(JHU_UPDATED_DATA_FILENAME, "wb")
+            csv_file = open(input_csv, "wb")
             csv_file.write(content)
             csv_file.close()
-            print("successfully updated {}".
-                  format(JHU_UPDATED_DATA_FILENAME))
+            print("successfully updated {}".format(input_csv))
 
     @staticmethod
     def usage():
@@ -323,37 +344,29 @@ if __name__ == "__main__":
     print("current working directory: {}".format(os.getcwd()))
     os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
-    if len(argv) < 2:
+    if len(argv) < 5:
         CovidData.usage()
         exit(1)
-
-    for param in argv:
-        if param == "--update":
-            CovidData.update_local_data()
-        if param == "--help":
-            CovidData.usage()
 
     if argv[1].lower() == "us" or argv[1].lower() == "usa":
         country = "US"
     else:
         country = argv[1].capitalize()
-    if argv[2] == "":
-        module = "bokeh"
-    else:
-        module = argv[2].lower()
 
-    if len(argv) == 4 and argv[3] != "--update" and argv[3] != "--help":
-        output = argv[3]
-        print("Don't forget to push your website in order to"
-              + "upload the latest changes made to deaths.html")
-    elif len(argv) == 5:
-        if argv[3] != "--update" and argv[3] != "--help":
-            output = argv[3]
-        else:
-            output = argv[4]
-    else:
-        output = "deaths.html"
+    module = argv[2].lower()
+    input_csv = argv[3]
+    output = argv[4]
 
-    covid_data = CovidData()
+    for param in argv:
+        if param == "--update":
+            if "deaths" in input_csv:
+                source = "deaths"
+            else:
+                source = "confirmed"
+            CovidData.update_local_data(source, input_csv)
+        if param == "--help":
+            CovidData.usage()
+
+    covid_data = CovidData(input_csv)
     covid_data.select_country(name=country)
     covid_data.plot_selected_country(name=country, output=output, module=module)
